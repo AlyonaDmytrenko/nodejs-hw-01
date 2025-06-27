330225fd7cf56cf1c904a854275f21a3ecb31c50
unknown <dmytrenkoalyona@gmail.com>
2025-06-27 21:07:28 +0300

    add read, write

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
+// import { error } from 'node:console';
 import { PATH_DB } from '../constants/contacts.js';
+
+const fs = require('node:fs/promises');
+fs.readFile("db.json", {encoding: "utf-8"})
+.then(data => {
+    console.log(data);
+})
+.catch(error=>{
+    console.error(error);
+})
 
 export const readContacts = async () => {};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
 
+
+const fs = require('node:fs/promises');
+fs.writeFile("db.json", [])
+.then(() => {
+    console.log("ok");
+})
+.catch(error=>{
+    console.error(error);
+})
+
 export const writeContacts = async (updatedContacts) => {};
