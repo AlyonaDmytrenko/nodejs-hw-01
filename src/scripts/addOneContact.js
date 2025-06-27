16db74263ff481ed346c181067b114f8fb4030f9
unknown <dmytrenkoalyona@gmail.com>
2025-06-27 22:00:44 +0300

    addOneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,13 @@
 export const addOneContact = async () => {};
 
+const fs = require('node:fs/promises');
+
+fs.appendFile("db.json", [])
+.then(() => {
+    console.log("ok");
+})
+.catch(error=>{
+    console.error(error);
+})
+
 addOneContact();
